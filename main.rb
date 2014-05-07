5737d552845edd4b9bd22690e703f3c2c49651e0
Tyler Boyd <tboyd@rentpath.com>
2014-05-07 15:55:50 -0400

    use .xtop.rb, add sample file

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 Dir.glob(File.expand_path('../xtop_*.rb', __FILE__)).each { |f| require f }
 
-view = XtopView.new({
-  "google.com" => proc { sleep Random.rand(5)/4.0; :ok },
-  "yahoo.com" => proc { :error },
-  "bing.com" => proc { :ok }
-})
+file = `pwd`.chomp + "/.xtop.rb"
+
+if File.exists?(file)
+  view = XtopView.new(eval(File.read(file)))
+elsif File.exists?(File.expand_path("~/.xtop.rb"))
+  view = XtopView.new(eval(File.read(File.expand_path("~/.xtop.rb"))))
+else
+  exit(0) # need .xtop.rb file
+end
 
 XtopLoop.start(view) do |key|
   case key
